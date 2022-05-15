2f425779e1d46917ef7a6cc78d0a8efc4ab3e50c
sethu1012 <sethu10121994@gmail.com>
2022-05-15 22:22:36 +0530

    :sparkles: added file download feature

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import server from "fastify";
+import fastifyCors from "@fastify/cors";
 import { fabric } from "fabric";
-import fs, { existsSync, mkdirSync } from "fs";
+import { createReadStream, existsSync, mkdirSync } from "fs";
 import jsPDF from "jspdf";
 import { IStaticCanvasOptions } from "fabric/fabric-impl";
 import path from "path";
+import { nanoid } from "nanoid";
+import AdmZip from "adm-zip";
 
 var faker = require("@faker-js/faker").faker;
 
@@ -13,69 +16,111 @@ interface IFabricObj {
   count: number;
 }
 
+interface IQueryString {
+  name: string;
+  folder: string;
+}
+
 const fastify = server({ logger: true });
+
+fastify.register(fastifyCors, {
+  origin: "*",
+});
+
+fastify.get<{
+  Querystring: IQueryString;
+}>("/", async (request, reply) => {
+  const { folder, name }: IQueryString = request.query;
+  const filePath = path.join(__dirname, "..", "data", folder, name + ".zip");
+  console.log(filePath, existsSync(filePath));
+  if (existsSync(filePath)) {
+    const stream = createReadStream(filePath);
+    return reply
+      .header("Content-Disposition", `attachment;`)
+      .header("Content-Type", "application/zip")
+      .send(stream);
+  } else {
+    return reply.status(404).send();
+  }
+});
 
 fastify.post<{
   Body: IFabricObj;
 }>("/", async (request, reply) => {
-  const { fabricObj, name = "", count = 0 }: IFabricObj = request.body;
-  const pageCount = fabricObj.length;
-  const time = new Date().getTime();
-  for (let i = 0; i < count; i++) {
-    const doc = new jsPDF();
-    fabricObj.forEach((page, index) => {
-      const fab: IStaticCanvasOptions = page;
-      const { height, width } = fab.backgroundImage as fabric.Image;
-      const canvas = new fabric.StaticCanvas(null, {
-        width: width,
-        height: height,
-      });
-      canvas.loadFromJSON(
-        fab,
-        function () {
-          canvas.renderAll();
-
-          doc.addImage(
-            canvas.toDataURL({ format: "png" }),
-            "PNG",
-            0,
-            0,
-            doc.internal.pageSize.getWidth(),
-            doc.internal.pageSize.getHeight(),
-            `page-${index + 1}`,
-            "FAST"
-          );
-          if (index === pageCount - 1) {
-            const fileName = `${name?.trim()} - ${i + 1}.pdf`;
-            const fileData = doc.output();
-            const outputDir = path.join(
-              __dirname,
-              "..",
-              "data",
-              time.toString()
-            );
-            if (!existsSync(outputDir)) {
-              mkdirSync(outputDir, { recursive: true });
-            }
-            const filePath = path.join(outputDir, fileName);
-            fs.writeFileSync(filePath, fileData, "binary");
-          }
-        },
-        function (o: any, object: any) {
-          // new Promise(function (resolve, reject) {
-          const type = (object as fabric.Object).get("type");
-          if (type === "i-text") {
-            object.set({ text: eval(o["expression"]) });
-            // resolve(true);
-          }
-          // });
+  try {
+    const { fabricObj, name = "File", count = 0 }: IFabricObj = request.body;
+    console.log("\n\n\n\n\nName: " + request.body.name + " \n\n\n\n\n");
+    const folder = nanoid();
+    const zip = new AdmZip();
+    const allPromises: any = [];
+    for (let i = 0; i < count; i++) {
+      const doc = new jsPDF();
+      let index = 0;
+      for (let page of fabricObj) {
+        const fab: IStaticCanvasOptions = page;
+        const { height, width } = fab.backgroundImage as fabric.Image;
+        const canvas = new fabric.StaticCanvas(null, {
+          width: width,
+          height: height,
+        });
+        allPromises.push(await loadJSON(canvas, fab, doc, index++));
+      }
+      Promise.all(allPromises).then(() => {
+        const fileName = `${name.trim()} - ${i + 1}.pdf`;
+        const fileData = doc.output();
+        const outputDir = path.join(__dirname, "..", "data", folder);
+        if (!existsSync(outputDir)) {
+          mkdirSync(outputDir, { recursive: true });
         }
-        // );
-      );
-    });
+        zip.addFile(fileName, Buffer.from(fileData, "binary"), "", 0o0644);
+        const zipPath = path.join(outputDir, name.trim() + ".zip");
+        zip.writeZip(zipPath);
+      });
+    }
+    return reply.status(200).send({ folder, name: name.trim() });
+  } catch (e) {
+    console.error(e);
+    return reply.status(400).send();
   }
-  return { hello: "world" };
 });
+
+async function loadJSON(
+  canvas: fabric.StaticCanvas,
+  fab: IStaticCanvasOptions,
+  doc: jsPDF,
+  index: number
+) {
+  return new Promise((resolve) => {
+    canvas.loadFromJSON(
+      fab,
+      function () {
+        canvas.renderAll();
+
+        if (index != 0) {
+          doc.addPage();
+          doc.setPage(index + 1);
+        }
+        doc.addImage(
+          canvas.toDataURL({ format: "png" }),
+          "PNG",
+          0,
+          0,
+          doc.internal.pageSize.getWidth(),
+          doc.internal.pageSize.getHeight(),
+          `page-${index + 1}`,
+          "FAST"
+        );
+        resolve(true);
+      },
+      function (o: any, object: any) {
+        const type = (object as fabric.Object).get("type");
+        if (type === "i-text" && o["expression"]) {
+          object.set({ text: eval(o["expression"]) });
+        }
+      }
+    );
+  });
+}
 
 // Run the server!
 const start = async () => {
